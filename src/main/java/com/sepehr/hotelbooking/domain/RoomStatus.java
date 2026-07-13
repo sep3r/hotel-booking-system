2aed41334d63e9ba3cc84b4e33abc66bc833e0f9
sep3r <sep3r@yahoo.com>
2026-07-13 11:59:53 -0700

    fea(domain): add RoomStatus

---
+++ src/main/java/com/sepehr/hotelbooking/domain/RoomStatus.java
@@ -0,0 +1,8 @@
+package com.sepehr.hotelbooking.domain;
+
+public enum RoomStatus {
+
+    AVAILABLE,
+    MAINTENANCE,
+    OUT_OF_SERVICE
+}
---
+++ src/main/java/com/sepehr/hotelbooking/domain/RoomType.java
@@ -0,0 +1,9 @@
+package com.sepehr.hotelbooking.domain;
+
+public enum RoomType {
+
+    SINGLE,
+    DOUBLE,
+    SUITE,
+    DELUXE
+}
