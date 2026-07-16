303c05267f1795f620d93b3c2089254b99f57778
sep3r <sep3r@yahoo.com>
2026-07-16 04:26:43 -0700

    feat(service): add userservice and userserviceimpl

--- src/main/java/com/sepehr/hotelbooking/service/UserService.java
+++ src/main/java/com/sepehr/hotelbooking/service/UserService.java
@@ -1,4 +1,25 @@
 package com.sepehr.hotelbooking.service;
 
+import com.sepehr.hotelbooking.domain.User;
+import com.sepehr.hotelbooking.dto.request.CreateUserRequest;
+
+import java.util.List;
+
+
 public interface UserService {
+
+
+    User createUser(CreateUserRequest request);
+
+
+    User getUserById(Long id);
+
+
+    User getUserByEmail(String email);
+
+
+    List<User> getAllUsers();
+
+
+    void deleteUser(Long id);
 }
--- src/main/java/com/sepehr/hotelbooking/service/impl/UserServiceImpl.java
+++ src/main/java/com/sepehr/hotelbooking/service/impl/UserServiceImpl.java
@@ -1,4 +1,83 @@
 package com.sepehr.hotelbooking.service.impl;
 
-public class UserServiceImpl {
+
+import com.sepehr.hotelbooking.domain.User;
+import com.sepehr.hotelbooking.dto.request.CreateUserRequest;
+import com.sepehr.hotelbooking.exception.ResourceNotFoundException;
+import com.sepehr.hotelbooking.repository.UserRepository;
+import com.sepehr.hotelbooking.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserServiceImpl implements UserService {
+
+
+    private final UserRepository userRepository;
+
+
+    @Override
+    @Transactional
+    public User createUser(CreateUserRequest request) {
+
+
+        User user = new User(
+                request.getFirstName(),
+                request.getLastName(),
+                request.getEmail(),
+                request.getPassword()
+        );
+
+
+        return userRepository.save(user);
+    }
+
+
+    @Override
+    public User getUserById(Long id) {
+
+        return userRepository.findById(id)
+                .orElseThrow(
+                        () -> new ResourceNotFoundException(
+                                "User not found with id: " + id
+                        )
+                );
+    }
+
+
+    @Override
+    public User getUserByEmail(String email) {
+
+        return userRepository.findByEmail(email)
+                .orElseThrow(
+                        () -> new ResourceNotFoundException(
+                                "User not found with email: " + email
+                        )
+                );
+    }
+
+
+    @Override
+    public List<User> getAllUsers() {
+
+        return userRepository.findAll();
+    }
+
+
+    @Override
+    @Transactional
+    public void deleteUser(Long id) {
+
+        User user = getUserById(id);
+
+        userRepository.delete(user);
+    }
 }
