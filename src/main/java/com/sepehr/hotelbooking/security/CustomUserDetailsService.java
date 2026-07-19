e527ed5621e6ec9b77c24cec997480fce2aa5f1e
sep3r <sep3r@yahoo.com>
2026-07-19 14:05:41 -0700

    chore(security): remove white space from CustomUserDetailsService

--- src/main/java/com/sepehr/hotelbooking/security/CustomUserDetailsService.java
+++ src/main/java/com/sepehr/hotelbooking/security/CustomUserDetailsService.java
@@ -16,34 +16,20 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 
-
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-
     private final UserRepository userRepository;
-
-
 
     @Override
     public UserDetails loadUserByUsername(String email) {
-
-
         User user = userRepository.findByEmail(email)
                 .orElseThrow(
                         () -> new UsernameNotFoundException(
                                 "User not found with email: " + email
                         )
                 );
-
-
-        return org.springframework.security.core.userdetails.User
-                .builder()
-                .username(user.getEmail())
-                .password(user.getPassword())
-                .roles(user.getRole().name())
-                .build();
-
+        return new CustomUserDetails(user);
     }
 }
