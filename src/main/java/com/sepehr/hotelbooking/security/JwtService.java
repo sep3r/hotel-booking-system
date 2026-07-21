71ed6f3a64b9052408bcb42b15652221fda4e23f
sep3r <sep3r@yahoo.com>
2026-07-21 09:05:36 -0700

    feat(security): move jwt secret and expiration to yaml configuration

--- src/main/java/com/sepehr/hotelbooking/security/JwtService.java
+++ src/main/java/com/sepehr/hotelbooking/security/JwtService.java
@@ -1,39 +1,46 @@
 package com.sepehr.hotelbooking.security;
 
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import javax.crypto.SecretKey;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 
+
 @Service
+@RequiredArgsConstructor
 public class JwtService {
 
-    private static final String SECRET_KEY =
-            "my-super-secret-key-my-super-secret-key-123456";
+    @Value("${jwt.secret}")
+    private String secretKey;
 
-    private static final long EXPIRATION_TIME =
-            1000 * 60 * 60 * 24; // 24 hours
-
+    @Value("${jwt.expiration}")
+    private long expirationTime;
 
     private SecretKey getSignKey() {
+
         return Keys.hmacShaKeyFor(
-                SECRET_KEY.getBytes()
+                secretKey.getBytes()
         );
     }
 
     public String generateToken(String email) {
+
         return Jwts.builder()
                 .subject(email)
                 .issuedAt(new Date())
                 .expiration(
                         new Date(
                                 System.currentTimeMillis()
-                                        + EXPIRATION_TIME
+                                        + expirationTime
                         )
                 )
                 .signWith(getSignKey())
@@ -48,19 +55,16 @@ public class JwtService {
                         .build()
                         .parseSignedClaims(token)
                         .getPayload();
+
         return claims.getSubject();
     }
 
     public boolean isTokenValid(String token) {
 
         try {
-
             extractEmail(token);
-
             return true;
-
         } catch (Exception e) {
-
             return false;
         }
     }
